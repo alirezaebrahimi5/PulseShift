a850db0a8b24c11c98c6e857f85a878432cccd0b
Alireza Ebrahimi <ar.ebrahimi96@gmail.com>
2025-01-12 11:26:23 +0330

    init: init project

---
+++ pulseshift/src/test/java/com/pulseshift/PulseShiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pulseshift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulseShiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
